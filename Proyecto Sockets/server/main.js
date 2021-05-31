0306fa52e7cac7b67d1fc405ad4f0d6f9aadf32d
IsaacAlexis <alexxis.mal@gmail.com>
2021-05-31 12:30:31 -0600

    Update main.js

--- Proyecto Sockets/server/main.js
+++ Proyecto Sockets/server/main.js
@@ -7,6 +7,10 @@ app.get('/',function(req, res){
     res.status(200).send("Hola Mundo");
 });
 
+io.on('connection', function(socket){
+    console.log('Alguien se ha conectado con socket');
+});
+
 server.listen(3002,function(){
     console.log("El servidor esta corriendo en http://localhost:3002");
 });

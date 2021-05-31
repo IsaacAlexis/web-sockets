a3ce939f68124c5fd438fb5e85e8a147e47b52a2
IsaacAlexis <alexxis.mal@gmail.com>
2021-05-31 12:25:31 -0600

    1st Commit

---
+++ Proyecto Sockets/server/main.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.get('/',function(req, res){
+    res.status(200).send("Hola Mundo");
+});
+
+server.listen(3002,function(){
+    console.log("El servidor esta corriendo en http://localhost:3002");
+});

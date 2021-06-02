9f735857ec48c64f350e7cb1f5332a2ca692ecce
IsaacAlexis <alexxis.mal@gmail.com>
2021-06-02 09:32:08 -0600

    Adaptando server/main para los mensajes

--- Proyecto Sockets/server/main.js
+++ Proyecto Sockets/server/main.js
@@ -3,6 +3,12 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var messages = [{
+    id: 1,
+    texto:"Hola soy un mensaje",
+    autor: "Isaac Alexis Maldonado Holguin"
+}];
+
 app.use(express.static('public'));
 
 app.get('/',function(req, res){
@@ -12,10 +18,10 @@ app.get('/',function(req, res){
 io.on('connection', function(socket){
     console.log('Alguien se ha conectado con socket');
 
-    socket.emit('messages',{
-        id: 1,
-        texto:"Hola soy un mensaje",
-        autor: "Isaac Alexis Maldonado Holguin"
+    socket.emit('messages',messages);
+
+    socket.on('new-message', function(data){
+
     });
 });
 
